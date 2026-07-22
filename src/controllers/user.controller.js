cc6d30f67f1da3b0c349782a45dfdbabc5c383c6
Tejas Jagadale <tejas.mpeoples@gmail.com>
2026-07-22 10:38:10 +0530

    code update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,15 +1,8 @@
-import { User } from '../models/User.js';
-import { createCrudController } from '../utils/createCrudController.js';
-import { hashPassword } from '../utils/password.js';
-
 export const userController = createCrudController(User, {
   select: '-password',
   populate: ['dealerId', 'parentId', 'referredByUserId', 'createdBy'],
   searchableFields: ['name', 'username', 'email', 'phoneNumber'],
   filterableFields: ['role', 'dealerId', 'parentId', 'status', 'occupation'],
-  // DEALER accounts are scoped to users where dealerId === themselves.
-  // USER accounts are scoped to sub-users where parentId === themselves.
-  // ADMIN is unrestricted (not listed here).
   ownerScopes: {
     DEALER: 'dealerId',
     USER: 'parentId',
@@ -20,14 +13,19 @@ export const userController = createCrudController(User, {
       body.password = await hashPassword(body.password);
     }
     
-    // If the current user is creating a sub-user (role: SUB_USER),
-    // automatically set referredByUserId to the current user's ID
+    // If the current user is creating a sub-user (role: SUB_USER)
     if (req.user && body.role === 'SUB_USER') {
+      // Set the referring user
       body.referredByUserId = req.user._id;
       
-      // Also set parentId to the current user if not already set
+      // Set parentId to the current user (the one creating the sub-user)
       if (!body.parentId) {
         body.parentId = req.user._id;
+      }
+      
+      // Set dealerId from the parent user if not provided
+      if (!body.dealerId && req.user.dealerId) {
+        body.dealerId = req.user.dealerId;
       }
     }
     
