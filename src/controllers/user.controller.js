b3991fe602be3a525118c7296be79702cde86606
Tejas Jagadale <tejas.mpeoples@gmail.com>
2026-07-22 10:21:14 +0530

    code update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -14,13 +14,31 @@ export const userController = createCrudController(User, {
     DEALER: 'dealerId',
     USER: 'parentId',
   },
-  transformCreate: async (body) => {
+  transformCreate: async (body, req) => {
+    // Hash password if provided
     if (body.password) {
       body.password = await hashPassword(body.password);
     }
+    
+    // If the current user is creating a sub-user (role: SUB_USER),
+    // automatically set referredByUserId to the current user's ID
+    if (req.user && body.role === 'SUB_USER') {
+      body.referredByUserId = req.user._id;
+      
+      // Also set parentId to the current user if not already set
+      if (!body.parentId) {
+        body.parentId = req.user._id;
+      }
+    }
+    
+    // Set createdBy to the current user
+    if (req.user) {
+      body.createdBy = req.user._id;
+    }
+    
     return body;
   },
-  transformUpdate: async (body) => {
+  transformUpdate: async (body, req) => {
     if (body.password) {
       body.password = await hashPassword(body.password);
     }
