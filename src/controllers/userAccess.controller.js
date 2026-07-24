110351f85366596dece75f38637d1aa0e56ada2f
Tejas Jagadale <tejas.mpeoples@gmail.com>
2026-07-24 12:26:04 +0530

    update code

--- src/controllers/userAccess.controller.js
+++ src/controllers/userAccess.controller.js
@@ -4,4 +4,10 @@ import { createCrudController } from '../utils/createCrudController.js';
 export const userAccessController = createCrudController(UserAccess, {
   populate: ['dealerId', 'ownerUserId', 'sharedUserId', 'createdBy'],
   filterableFields: ['dealerId', 'ownerUserId', 'sharedUserId', 'status'],
+  // Add ownerScopes to control access based on role
+  ownerScopes: {
+    USER: 'ownerUserId',    // USER can only access their own user_access records
+    DEALER: 'dealerId',     // DEALER can access records for their dealer
+    ADMIN: null,            // ADMIN can access all
+  },
 });
